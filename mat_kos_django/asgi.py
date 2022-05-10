60662f6f0f68efe8665773bf0bfa11602ea76119
Mateusz Kostrzewa <mtkstr@gmail.com>
2022-05-10 22:20:16 +0200

    add seo app initial commit

--- mat_kos_django/asgi.py
+++ mat_kos_django/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for vercel_app project.
+ASGI config for mat_kos_django project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vercel_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mat_kos_django.settings')
 
 application = get_asgi_application()
--- mat_kos_django/wsgi.py
+++ mat_kos_django/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for vercel_app project.
+WSGI config for mat_kos_django project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vercel_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mat_kos_django.settings')
 
 application = get_wsgi_application()
